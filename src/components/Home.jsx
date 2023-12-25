50626f6d47206433755909066d476084f807916d
yu1chiro <takumiharu25@gmail.com>
2023-12-25 13:44:03 +0800

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
-import { useHistory } from 'react-router-dom'; // Assuming you are using React Router
 import Typewriter from 'typewriter-effect';
 
 const Home = () => {
-  const history = useHistory();
-
   const handleButtonClick = () => {
-    // Navigate to "kelas work"
-    history.push('work'); // Replace '/kelas-work' with the actual path you want to navigate to
+    // Change the URL to the path where your Work.jsx component is located
+    window.location.href = '../components/Work.jsx'; // Replace '/path-to-work' with the actual path
   };
 
   return (
     <div name="home" className="w-full h-screen bg-gradient-to-l from-[#020617] to-[#0a37a1]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        {/* ... (your existing code) */}
+        <p className="text-white text-lg">Hello my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#00FFCA]">
+          Hera
+        </h1>
+        <div>
+          <h2 className="text-2xl sm:text-4xl font-bold text-zinc-50 pt-2 flex">
+            <Typewriter
+              options={{
+                loop: true,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Canva designer")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Junior Web Developer")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Japanese Education Student")
+                  .start();
+              }}
+            />
+          </h2>
+        </div>
+        <p className="text-white text-lg py-4 leading-8">
+          I'm a junior web developer and Japanese language education student, let's be friends.🖐️
+        </p>
         <div>
           <button
             onClick={handleButtonClick}
             className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#00FFCA] hover:border-[#00FFCA] rounded-sm hover:text-[#3A1078] font-semibold"
           >
-            My Project
+            My Project 
             <span className="group-hover:translate-x-1 duration-300">
               {" "}
               <HiArrowNarrowRight className="ml-4" />
@@ -31,6 +54,6 @@ const Home = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Home;
